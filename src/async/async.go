6f5e0955da33aba1d66dab30d3e62f6c6184af98
ninghf <nhf228@msn.cn>
2018-04-20 18:04:16 +0800

    补充编解码函数

--- src/async/async.go
+++ src/async/async.go
@@ -1,6 +1,11 @@
 package async
 
-import "io"
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+const async_head_len = 16
 
 type async_head struct {
 	version uint32
@@ -9,8 +14,20 @@ type async_head struct {
 	dstid   uint32
 }
 
-func decode(src io.Reader) *async_head {
+func decode(buf *bytes.Buffer) *async_head {
 	head := new(async_head)
-
+	binary.Read(buf, binary.LittleEndian, head.version)
+	binary.Read(buf, binary.LittleEndian, head.msgid)
+	binary.Read(buf, binary.LittleEndian, head.srcid)
+	binary.Read(buf, binary.LittleEndian, head.dstid)
 	return head
+}
+
+func encode(head *async_head) *bytes.Buffer {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, head.version)
+	binary.Write(buf, binary.LittleEndian, head.msgid)
+	binary.Write(buf, binary.LittleEndian, head.srcid)
+	binary.Write(buf, binary.LittleEndian, head.dstid)
+	return buf
 }
