46c9a04f3811300a2537b2a84acb20cdee51482c
ninghf <nhf228@msn.cn>
2018-04-20 19:24:46 +0800

    修改函数包外调用

--- src/async/async.go
+++ src/async/async.go
@@ -3,6 +3,8 @@ package async
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
+	"log"
 )
 
 const async_head_len = 16
@@ -14,20 +16,48 @@ type async_head struct {
 	dstid   uint32
 }
 
-func decode(buf *bytes.Buffer) *async_head {
+func Decode(buf *bytes.Buffer) *async_head {
 	head := new(async_head)
-	binary.Read(buf, binary.LittleEndian, head.version)
-	binary.Read(buf, binary.LittleEndian, head.msgid)
-	binary.Read(buf, binary.LittleEndian, head.srcid)
-	binary.Read(buf, binary.LittleEndian, head.dstid)
+	err_version := binary.Read(buf, binary.LittleEndian, head.version)
+	if err_version != nil {
+		log.Println(err_version)
+	}
+	err_msgid := binary.Read(buf, binary.LittleEndian, head.msgid)
+	if err_msgid != nil {
+		log.Println(err_msgid)
+	}
+	err_srcid := binary.Read(buf, binary.LittleEndian, head.srcid)
+	if err_srcid != nil {
+		log.Println(err_srcid)
+	}
+	err_dstid := binary.Read(buf, binary.LittleEndian, head.dstid)
+	if err_dstid != nil {
+		log.Println(err_dstid)
+	}
+	fmt.Println(head.version)
+	fmt.Println(head.msgid)
+	fmt.Println(head.srcid)
+	fmt.Println(head.dstid)
 	return head
 }
 
-func encode(head *async_head) *bytes.Buffer {
+func Encode(head *async_head) *bytes.Buffer {
 	buf := new(bytes.Buffer)
-	binary.Write(buf, binary.LittleEndian, head.version)
-	binary.Write(buf, binary.LittleEndian, head.msgid)
-	binary.Write(buf, binary.LittleEndian, head.srcid)
-	binary.Write(buf, binary.LittleEndian, head.dstid)
+	err_version := binary.Write(buf, binary.LittleEndian, head.version)
+	if err_version != nil {
+		log.Println(err_version)
+	}
+	err_msgid := binary.Write(buf, binary.LittleEndian, head.msgid)
+	if err_msgid != nil {
+		log.Println(err_msgid)
+	}
+	err_srcid := binary.Write(buf, binary.LittleEndian, head.srcid)
+	if err_srcid != nil {
+		log.Println(err_srcid)
+	}
+	err_dstid := binary.Write(buf, binary.LittleEndian, head.dstid)
+	if err_dstid != nil {
+		log.Println(err_dstid)
+	}
 	return buf
 }
